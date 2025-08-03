937cf2fbf0d1fac0ba585fb7bd5c28fc1f3af310
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-08-03 13:48:44 +0200

    feat: create audioToTextUseCase.ts file and integrate

---
+++ src/core/use-cases/audio-to-text.use-case.ts
@@ -0,0 +1,29 @@
+import { AudioToTextResponse } from "../../interfaces";
+
+export const audioToTextUseCase = async (audioFile: File, prompt?: string) => {
+  try {
+    const formData = new FormData();
+
+    formData.append("file", audioFile);
+    if (prompt) {
+      formData.append("prompt", prompt);
+    }
+    const resp = await fetch(`${import.meta.env.VITE_GPT_API}/audio-to-text`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!resp.ok) throw new Error("No se pudo realizar la transcripción");
+    const data = (await resp.json()) as AudioToTextResponse;
+
+    return {
+      ok: true,
+      message: data.text,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      message: error,
+    };
+  }
+};
--- src/presentation/pages/audio-to-text/AudioToTextPage.tsx
+++ src/presentation/pages/audio-to-text/AudioToTextPage.tsx
@@ -1,3 +1,71 @@
+import { useState } from "react";
+
+/* Components */
+import {
+  GptMessage,
+  MyMessage,
+  TypingLoader,
+  TextMessageBoxFile,
+} from "../../components";
+import { audioToTextUseCase } from "../../../core/use-cases";
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
+
 export function AudioToTextPage() {
-  return <div>AudioToTextPage</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const hanlderPost = async (text: string, audioFile: File) => {
+    setIsLoading(true);
+
+    setMessages((prev) => [...prev, { text: text, isGpt: false }]);
+
+    const resp = await audioToTextUseCase(audioFile, text);
+    setIsLoading(false);
+
+    if (!resp.ok) return;
+
+    const gptMessage = `
+    ## El texto es:
+    ${resp.message}
+    `;
+
+    setMessages((prev) => [...prev, { text: gptMessage, isGpt: true }]);
+  };
+
+  return (
+    <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-white/5 h-full p-4">
+      <div className="flex flex-col h-full overflow-y-auto mb-4">
+        <div className="grid grid-cols-12 gap-y-2">
+          <div className="col-span-12">
+            <GptMessage text="Hola, que audio quieres convertir a texto?" />
+          </div>
+
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <div className="col-span-12" key={index}>
+                <GptMessage text={message.text} />
+              </div>
+            ) : (
+              <div className="col-span-12" key={index}>
+                <MyMessage key={index} text={message.text} />
+              </div>
+            )
+          )}
+
+          {isLoading && <TypingLoader className="fade-in" />}
+        </div>
+      </div>
+
+      <TextMessageBoxFile
+        onSendMessage={hanlderPost}
+        placeholder="Escribe aquí lo que deseas"
+        disabledCorrection
+        accept="audio/*"
+      />
+    </div>
+  );
 }
