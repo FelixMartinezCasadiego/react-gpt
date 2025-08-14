ddbdece1416b01072cc9f8634d71bccb33a24915
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-08-14 20:45:30 +0200

    feat: integrate endpoint about image generation and update ImageGenerationPage.tsx file

--- src/presentation/pages/image-generation/ImageGenerationPage.tsx
+++ src/presentation/pages/image-generation/ImageGenerationPage.tsx
@@ -1,3 +1,83 @@
+import { useState } from "react";
+
+/* Components */
+import {
+  GptMessage,
+  GptMessageImage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader,
+} from "../../components";
+import { imageGenerationUseCase } from "../../../core/use-cases";
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+  info?: { imageUrl: string; alt: string };
+}
+
 export function ImageGenerationPage() {
-  return <div>ImageGenerationPage</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const hanlderPost = async (text: string) => {
+    setIsLoading(true);
+
+    setMessages((prev) => [...prev, { text: text, isGpt: false }]);
+
+    const imageInfo = await imageGenerationUseCase(text);
+
+    setIsLoading(false);
+
+    if (!imageInfo) {
+      return setMessages((prev) => [
+        ...prev,
+        { text: "No se pudo generar la iamgen", isGpt: true },
+      ]);
+    }
+
+    setMessages((prev) => [
+      ...prev,
+      {
+        text: text,
+        isGpt: true,
+        info: { imageUrl: imageInfo.url, alt: imageInfo.alt },
+      },
+    ]);
+  };
+
+  return (
+    <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-white/5 h-full p-4">
+      <div className="flex flex-col h-full overflow-y-auto mb-4">
+        <div className="grid grid-cols-12 gap-y-2">
+          <div className="col-span-12">
+            <GptMessage text="Que imagen deseas generar hoy?" />
+          </div>
+
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <div className="col-span-12" key={index}>
+                <GptMessageImage
+                  imageUrl={message.info?.imageUrl!}
+                  alt={message.info?.alt!}
+                />
+              </div>
+            ) : (
+              <div className="col-span-12" key={index}>
+                <MyMessage key={index} text={message.text} />
+              </div>
+            )
+          )}
+
+          {isLoading && <TypingLoader className="fade-in" />}
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={hanlderPost}
+        placeholder="Escribe aquí lo que deseas"
+        disabledCorrection={true}
+      />
+    </div>
+  );
 }
